f46c7825f09834672fc44acaf89b4391ef9cc7ca
STeve Huang <xin.huang@goteleport.com>
2024-12-10 21:51:39 -0500

    GitHub proxy part 6.5: tsh git ssh/clone/config

---
+++ tool/tsh/common/git_test.go
@@ -0,0 +1,65 @@
+/*
+ * Teleport
+ * Copyright (C) 2024  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_parseGitSSHURL(t *testing.T) {
+	tests := []struct {
+		input     string
+		wantError bool
+		wantOut   *gitSSHURL
+	}{
+		{
+			input: "org-1234567@github.com:some-org/some-repo.git",
+			wantOut: &gitSSHURL{
+				user:  "org-1234567",
+				host:  "github.com",
+				owner: "some-org",
+				path:  "some-org/some-repo.git",
+			},
+		},
+		{
+			input: "ssh://git@github.com/some-org/some-repo.git",
+			wantOut: &gitSSHURL{
+				user:  "git",
+				host:  "github.com",
+				owner: "some-org",
+				path:  "some-org/some-repo.git",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			out, err := parseGitSSHURL(tt.input)
+			t.Log(out, err)
+			if tt.wantError {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.wantOut, out)
+		})
+	}
+}
