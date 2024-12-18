afb83d666ca929f0c445a31a2e7d5fef39552d71
joerger <bjoerger@goteleport.com>
2024-12-18 11:31:58 -0800

    Use global mfa context for all admin actions with explicit MFA checks.

--- web/packages/teleport/src/services/api/api.ts
+++ web/packages/teleport/src/services/api/api.ts
@@ -19,22 +19,15 @@
 import websession from 'teleport/services/websession';
 import 'whatwg-fetch';
 
-import { MfaContextValue } from 'teleport/MFAContext/MFAContext';
-
 import { MfaChallengeResponse } from '../mfa';
 import { storageService } from '../storageService';
+import auth from '../auth/auth';
 
 import parseError, { ApiError } from './parseError';
 
 export const MFA_HEADER = 'Teleport-Mfa-Response';
 
-let mfaContext: MfaContextValue;
-
 const api = {
-  setMfaContext(mfa: MfaContextValue) {
-    mfaContext = mfa;
-  },
-
   get(
     url: string,
     abortSignal?: AbortSignal,
@@ -196,7 +189,7 @@ const api = {
 
     let mfaResponseForRetry;
     try {
-      mfaResponseForRetry = await mfaContext.getAdminActionMfaResponse();
+      mfaResponseForRetry = await auth.getAdminActionMfaResponse();
     } catch {
       throw new Error(
         'Failed to fetch MFA challenge. Please connect a registered hardware key and try again. If you do not have a hardware key registered, you can add one from your account settings page.'
