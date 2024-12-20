82fea9fc613a0bc8765b84b6134ee866741621e9
Dustin Specker <dustin.specker@goteleport.com>
2024-12-20 09:21:45 -0600

    feat(api/sessionrecording): move lib/events' ProtoReader
    This moves ProtoReader from lib/events to
    api/sessionrecording so that it may be imported by users.

---
+++ api/sessionrecording/stream_test.go
@@ -0,0 +1,31 @@
+package sessionrecording_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/sessionrecording"
+)
+
+// TestReadCorruptedRecording tests that the streamer can successfully decode the kind of corrupted
+// recordings that some older bugged versions of teleport might end up producing when under heavy load/throttling.
+func TestReadCorruptedRecording(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	f, err := os.Open("testdata/corrupted-session")
+	require.NoError(t, err)
+	defer f.Close()
+
+	reader := sessionrecording.NewProtoReader(f)
+	defer reader.Close()
+
+	events, err := reader.ReadAll(ctx)
+	require.NoError(t, err)
+
+	// verify that the expected number of events are extracted
+	require.Len(t, events, 12)
+}
